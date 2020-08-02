a84b17f2f391134f10b06d0d663b0fe4fba6a44a
smallcluster <small.cluster@gmail.com>
2020-08-02 21:13:17 +0400

    Update cross_val.py
    Datasets are loaded when evaluating, now the script is less memory hungry

--- cross_val.py
+++ cross_val.py
@@ -28,31 +28,37 @@ nb_split = 10
 
 # --------------- MAIN PROGRAM ---------------------------------
 
-# Loads data for cross validation
-# [ [data, class, dataset name], ...]
-data = []
-for name in os.listdir(DATA_PATH):
-    print("Loading %s dataset..." % name)
-    start_time = time.perf_counter()
+# Display some info
+nbDatasets = 0
+for  dataset in os.listdir(DATA_PATH): nbDatasets+=1
+print("Evaluating on %s cores %d classifiers on %d datasets with a %d fold cross-validation..." % 
+     ("all" if nb_jobs == -1 else str(nb_jobs), len(classifiers), nbDatasets, nb_split))
+
+# Evaluates all classifiers using cross validation per dataset
+for dataset in os.listdir(DATA_PATH):
+    # Loads dataset for cross validation
+    print("\nLoading %s dataset..." % dataset)
+    start_load_time = time.perf_counter()
+
+    filepath = dataset+"/"+dataset+"_" # dataset/dataset_TEST.ts and dataset/dataset_TRAIN.ts
     # Load train data + class
-    d, c = load_from_tsfile_to_dataframe(os.path.join(DATA_PATH, name+"/"+name+"_TRAIN.ts"))
+    d, c = load_from_tsfile_to_dataframe(os.path.join(DATA_PATH, filepath+"TRAIN.ts"))
     # Load test data + class
-    dd, cc = load_from_tsfile_to_dataframe(os.path.join(DATA_PATH, name+"/"+name+"_TEST.ts"))
-    # Store all data, all class and dataset name
-    data+=[[d.append(dd),np.concatenate((c, cc)), name]]
+    dd, cc = load_from_tsfile_to_dataframe(os.path.join(DATA_PATH, filepath+"TEST.ts"))
 
-    elapsed_time = time.perf_counter() - start_time
-    print("Finished in: %f seconds" % elapsed_time)
+    # Store all data and all class (concatenate train and test)
+    data, classes = d.append(dd), np.concatenate((c, cc))
 
-# Evaluates classifiers using cross validation per datasets
-for classifier, classifier_name in classifiers:
-    print("|---Classifier: "+classifier_name)
-    for (d, c, name) in data:
-        print("    |---Dataset: "+name)
+    elapsed_load_time = time.perf_counter() - start_load_time
+    print("Loading took: %f seconds" % elapsed_load_time)
+
+    # Now we do all cross-validatiosn on this dataset
+    for classifier, classifier_name in classifiers:
+        print("Classifier: "+classifier_name)
         start_time = time.perf_counter()
-        scores = cross_val_score(classifier, d, c, cv=nb_split, n_jobs=nb_jobs)
+        scores = cross_val_score(classifier, data, classes, cv=nb_split, n_jobs=nb_jobs)
         elapsed_time = time.perf_counter() - start_time
-        print("        |---Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-        print("        |---Finished in: %f seconds" % elapsed_time)
+        print("    |Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+        print("    |_Cross validation took: %f seconds" % elapsed_time)
 
 print("Done.")
