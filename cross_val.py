a1919892947d9383e7647ddce63f34348ea459e8
smallcluster <small.cluster@gmail.com>
2020-08-02 21:19:17 +0400

    Update cross_val.py

--- cross_val.py
+++ cross_val.py
@@ -9,30 +9,33 @@ import sktime
 import time
 
 
-# -------------- SETUP ------------------------------------------
+# -------------- SETUP -----------------
 
-# Dataset path folder
+# Datasets path folder
 DATA_PATH = os.path.join(os.path.dirname(__file__), "Datasets")
 
-# Setup classifier
+# Classifiers
 # [ [classifier, classifier name], ...]
 classifiers = [
     [KNeighborsTimeSeriesClassifier(1, 'uniform', 'brute', 'dtw', None), "DTW-1NN"],
     [KNeighborsTimeSeriesClassifier(4, 'uniform', 'brute', 'dtw', None), "DTW-4NN"]
 ]
+
 # Number of cores to use (-1 -> all)
 nb_jobs = -1
 
 # Number of data split for cross validation
 nb_split = 10
 
-# --------------- MAIN PROGRAM ---------------------------------
+# --------------- MAIN PROGRAM --------------------
 
 # Display some info
 nbDatasets = 0
 for  dataset in os.listdir(DATA_PATH): nbDatasets+=1
 print("Evaluating on %s cores %d classifiers on %d datasets with a %d fold cross-validation..." % 
      ("all" if nb_jobs == -1 else str(nb_jobs), len(classifiers), nbDatasets, nb_split))
+
+start_global_time = time.perf_counter()
 
 # Evaluates all classifiers using cross validation per dataset
 for dataset in os.listdir(DATA_PATH):
@@ -61,4 +64,5 @@ for dataset in os.listdir(DATA_PATH):
         print("    |Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
         print("    |_Cross validation took: %f seconds" % elapsed_time)
 
-print("Done.")
+elapsed_global_time = time.perf_counter() - start_global_time
+print("\nDone in %f seconds." % elapsed_global_time)
